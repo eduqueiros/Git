4f73886b81aa699dd84c6a802eca259d220e6745
eduqueiros <eduardoqueiros10@gmail.com>
2022-09-30 10:29:14 +0100

    inicial commit4

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -2,19 +2,20 @@ package com.example.git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //changes
-        //adafdafa
-        //altera
-        //artur
-        //zeca
-        //something from branch funcionalidade1
-        //alterei te o projeto
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG", "onstart")
+
     }
     
 }
