3e4ea0f16f3cf0c78874c918e5cd503cf353681e
eduqueiros <eduardoqueiros10@gmail.com>
2022-09-30 11:11:44 +0100

    inicial commit5

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.git
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +16,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.d("TAG", "onstart")
+        Log.d("TAG", "onStart")
 
     }
-    
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAG", "onDestroy")
+    }
+
+    fun clickbotao(view: View) {
+
+        Toast.makeText(this, R.string.textlabel, Toast.LENGTH_SHORT).show()
+    }
+
+
 }
